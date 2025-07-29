4abfaa5d6e21fb6f5e51bad4225a86915dbde76b
FunWithNoFriends <jdzapf@gmail.com>
2025-07-29 13:15:10 -0700

    Create wordbank.js

---
+++ wordbank.js
@@ -0,0 +1,6 @@
+const wordBank = [
+  { answer: 'Witness Fitness', description: 'Someone observering – Physical training' },
+  { answer: 'Atomic Comic', description: 'An immeasurably small – Professional joker' },
+  { answer: 'Cricket Picket', description: 'Game with ball and bat – barriered by spaced uprights' },
+  { answer: 'Locket Rocket', description: 'An ornamental accessory – Containing its own propellant' },
+];
