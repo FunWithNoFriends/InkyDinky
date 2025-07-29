7e284a27b9b8049251aecda018ab5e320d767b6f
FunWithNoFriends <jdzapf@gmail.com>
2025-07-29 13:16:06 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,4 @@
 let guessedWords = [];
-  
-  const wordBank = [
-  { answer: 'Witness Fitness', description: 'Someone observering – Physical training' },
-  { answer: 'Atomic Comic', description: 'An immeasurably small – Professional joker' },
-  { answer: 'Cricket Picket', description: 'Game with ball and bat – barriered by spaced uprights' },
-  { answer: 'Locket Rocket', description: 'An ornamental accessory – Containing its own propellant' },
-];
 
 let current = {};
 let word1 = '', word2 = '';
